d8d96322a3bf839e3f6bdcbca21171cc690e51dc
fqjun <1767851382@qq.com>
2022-01-20 14:24:00 +0800

    feat: change staticPageGenerationTimeout to 90s

--- next.config.js
+++ next.config.js
@@ -13,5 +13,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = {
   // time in seconds of no pages generating during static
   // generation before timing out
-  staticPageGenerationTimeout: 600,
+  staticPageGenerationTimeout: 90,
 }
