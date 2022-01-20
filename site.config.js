1bb8bfe4bf1298b6a25e1daf2472978474532f4a
fqjun <1767851382@qq.com>
2022-01-20 15:25:39 +0800

    feat: test for pageUrlOverrides

--- site.config.js
+++ site.config.js
@@ -43,9 +43,19 @@ module.exports = {
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null
+  pageUrlOverrides: {
+    '/Manifold': 'f9714b72771c4949832733ba4e18c0f2',
+    '/Course': '386008a2c14d462d9ea9c317d23531af',
+    '/Conversion': 'bbc7b36ea94f4cdaa6eb0a1bfcd5aab7',
+    '/Knowledge-Base': 'a3f96f2aeb214d62b2df323351d66d15d23531af',
+    '/Method': '0a134da6916f4e46979d2c675f9acdb9',
+    '/Collection-Of-Resources': 'a05f5fa1f9fa45398e5ca6661311d273',
+    '/LaTeX': '1945754d78dc40de849c8d95a8c6e64b',
+    '/English': '08583c12feee46d8ab870906ce9e9a7a',
+    '/ToF': '362e9f5ae89c4ffab64e6bf6fbea53b1',
+    '/Camera': 'f826d34212224407ad268bdcfdd886ca',
+    '/Hard-Operation-manual-of-NUC-voltage-regulation': 'd9a16f93ff06466fa9488ed82c59ef18',
+
+  }
+  // pageUrlOverrides: null
 }
